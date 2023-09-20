8925b577df4665e3ac0511950a6a745856a89f56
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2023-09-21 00:03:32 +0200

    Update Proto2

--- Proto_2/Menu_burger/burger.js
+++ Proto_2/Menu_burger/burger.js
@@ -2,15 +2,21 @@ var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
 
-openBtn.onclick = openNav;
+openBtn.onclick = navAction
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
+function navAction() {
+  if (sidenav.classList.contains("active")) {
+    closeNav();
+  } else {
+    openNav();
+  }
+}
+
 function openNav() {
   sidenav.classList.add("active");
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   sidenav.classList.remove("active");
 }
